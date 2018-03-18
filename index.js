85da7cc272a51274f156961985985be3f0b5ab55
kfir marouani <kfir.marouani@my6sense.com>
2018-03-18 14:11:16 +0200

    Limited the handle movement

--- index.js
+++ index.js
@@ -11,10 +11,26 @@ document.addEventListener('DOMContentLoaded',function(){
 
     wrapper.addEventListener('mousemove',function (event) {
         delta = (event.clientX - window.innerWidth / 2) * 0.5;
+        // console.log('delta',delta);
 
         handle.style.left = event.clientX + delta + 'px';
+        if (event.clientX + delta > 1200){
+            handle.style.left = 1200 + 'px';
+        } else if (event.clientX + delta < 500) {
+            handle.style.left = 500 + 'px';
+        }
+
+        // console.log('handel left',handle.style.left);
 
         topLayer.style.width = event.clientX + skew + delta + 'px';
+
+        if (event.clientX + skew + delta > 2200){
+            topLayer.style.width = 2200 + 'px';
+        } else if (event.clientX + skew + delta < 1500) {
+            topLayer.style.width = 1500 + 'px';
+        }
+
+        // console.log('top width',event.clientX + skew + delta);
     });
 
 });
